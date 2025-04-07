4f269c39041256205e8bee4390d669aca38cb9c5
NeetuC05 <cneetu369@gmail.com>
2025-04-07 07:08:10 +0530

    Update one.py

--- one.py
+++ one.py
@@ -1,60 +1,28 @@
-# BFS
-from collections import deque
+!pip install scikit-learn
 
-def bfs(adj,s):
-    visited = [False]*len(adj)
-    q = deque([s])
-    visited[s] = True
-    while q:
-        curr = q.popleft()
-        print(chr(curr + ord("A")),end=" ")
-        for x in adj[curr]:
-            if not visited[x]:
-                visited[x] = True
-                q.append(x)
-
-def add_edge(adj,u,v):
-    adj[u].append(v)
-    adj[v].append(u)
-    
-# example 
-V = 7
-adj =[[] for _ in range(V)]
-edge = [(0,1),(0,2),(0,6),(2,3),(2,4),(2,5),(6,5)]
-for u,v in edge:
-    add_edge(adj,u,v)
-    
-print("BFS starting for 0:")
-bfs(adj,0)
+from sklearn.datasets import load_wine
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+data = load_wine()
+x_train, x_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.3, random_state=20)
+model = SVC()
+model.fit(x_train, y_train)
+y_pred = model.predict(x_test)
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy:{accuracy:.4f}")
 
 
-
-
-# DFS
-from collections import deque
-
-def dfs(adj,visited,s):
-    visited = True
-    print(chr(s +ord("A")),end=" ")
-    
-    for i in adj[s]:
-        if not visited[i]:
-            dfs_rec(adj,visited,i)
-
-def dfs(adj,s):
-    visited = [False]*len(adj)
-    dfs_rec(adj,visited,s)
-    
-def add_edge(adj,u,v):
-    adj[u].append(v)
-    adj[v].append(u)
-    
-# example 
-V=10
-adj=[[] for _ in range(V)]
-edge=[[0,1],[1,4],[4,5],[5,6],[6,5],[5,4],[4,3],[3,2],[2,0],[0,7],[7,8],[8,9]]
-for u,v in edge:
-    add_edge(adj,u,v)
-    
-print("DFS FROM SOURCE 1:")
-dfs(adj,0)
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score,mean_squared_error
+data = load_iris()
+x_train, x_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.3, random_state=20)
+model = RandomForestClassifier()
+model.fit(x_train, y_train)
+y_pred = model.predict(x_test)
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy:{accuracy:.4f}")
+mse = mean_squared_error(y_test, y_pred)
+print(f"Mean Squared Error:{mse:.4f}")
